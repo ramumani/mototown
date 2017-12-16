45cdd7900390a2d4f466d223c35bda745bbb75e9
drmani <drmani@drmani-PC>
2017-12-16 14:52:47 +0300

    Added 5

--- mototown/src/main/java/com/mototown/MototownApplication.java
+++ mototown/src/main/java/com/mototown/MototownApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MototownApplication {
 
 	public static void main(String[] args) {
-		System.out.println("1234..");
+		System.out.println("12345..");
 		SpringApplication.run(MototownApplication.class, args);
 	}
 }
