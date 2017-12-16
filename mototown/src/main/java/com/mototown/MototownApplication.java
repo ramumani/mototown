7fe068d93c0830667982cde945e412b7a8afcd40
drmani <drmani@drmani-PC>
2017-12-16 14:41:27 +0300

    Added second sysout

--- mototown/src/main/java/com/mototown/MototownApplication.java
+++ mototown/src/main/java/com/mototown/MototownApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MototownApplication {
 
 	public static void main(String[] args) {
-		System.out.println("123..");
+		System.out.println("1234..");
 		SpringApplication.run(MototownApplication.class, args);
 	}
 }
